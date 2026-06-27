dd1131df2d2481817b3ccc59eb304a0cfc1bf8b5
pachoritarun <pachoritarun05@gmail.com>
2026-06-27 09:46:54 +0530

    Allow hosts in vite preview

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     base: "/sessions/",
+    preview: {
+      allowedHosts: true,
+    }
   },
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
